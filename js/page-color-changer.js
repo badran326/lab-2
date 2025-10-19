90dc714bf54692e019904985a0bbe0b22d4fc7bc
Badr Ahmed <badr20126162@gmail.com>
2025-10-19 17:57:54 -0400

    Build one or more event handlers to call a function when the user changes the value of one of the range sliders.

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -2,3 +2,7 @@ const red = document.getElementById('red');;
 const green = document.getElementById('green');
 const blue = document.getElementById('blue');
 const body = document.body;
+
+red.addEventListener('input', changeBackgroundColor);
+green.addEventListener('input', changeBackgroundColor);
+blue.addEventListener('input', changeBackgroundColor);
