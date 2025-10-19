c653bae63b000c96e6169e6b228a5728dd1173b3
Badr Ahmed <badr20126162@gmail.com>
2025-10-19 18:14:16 -0400

    Build functions that change the background-color property value of the page body.

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -6,3 +6,11 @@ const body = document.body;
 red.addEventListener('input', changeBackgroundColor);
 green.addEventListener('input', changeBackgroundColor);
 blue.addEventListener('input', changeBackgroundColor);
+
+function changeBackgroundColor() {
+    const r = red.value;
+    const g = green.value;
+    const b = blue.value;
+    const backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    body.style.backgroundColor = backgroundColor;
+}
